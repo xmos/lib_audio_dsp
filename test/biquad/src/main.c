878dea568c4c5acd31e1594dfd0978bf26bc74e4
uvvpavel <paveluvarov@xmos.com>
2024-01-04 14:11:26 +0000

    fixing adsp_biquad, chanign biqaud test to take coeffs from python

--- test/biquad/src/main.c
+++ test/biquad/src/main.c
@@ -4,54 +4,43 @@
 #include <stdlib.h>
 #include "biquad.h"
 
-filter_biquad_s32_t filter = {
-  // Number of biquad sections in this filter block
-  .biquad_count = 1,
-  
-  // Filter state, initialized to 0
-  .state = {{0}},
-
-  // Filter coefficients
-  .coef = {{0}}
-};
-
-int main()
-{
-  int32_t taps_buf[5] = {0};
-  FILE * in = fopen("sig_48k.bin", "rb");
-  FILE * out = fopen("sig_out.bin", "wb");
-  if ((in == NULL) || (out == NULL))
+FILE * _fopen(char * fname, char* mode) {
+  FILE * fp = fopen(fname, mode);
+  if (fp == NULL)
   {
     printf("Error opening a file\n");
     exit(1);
   }
+  return fp;
+}
+
+int main()
+{
+  int32_t taps_buf[8] = {0};
+  int32_t state[8] = {0};
+  left_shift_t lsh = 0;
+
+  FILE * in = _fopen("sig_48k.bin", "rb");
+  FILE * out = _fopen("sig_out.bin", "wb");
+  FILE * coeffs = _fopen("coeffs.bin", "rb");
 
   fseek(in, 0, SEEK_END);
   int in_len = ftell(in) / sizeof(int32_t);
   fseek(in, 0, SEEK_SET);
 
-  float fs = 48000;
-  float f = 1000;
-  adsp_design_biquad_lowpass(taps_buf, f, fs, 0.7);
-  //adsp_design_biquad_highpass(taps_buf, f, fs, 0.7);
-  //adsp_design_biquad_notch(taps_buf, f, fs, 0.7);
-  //adsp_design_biquad_allpass(taps_buf, f, fs, 0.7);
-  //adsp_design_biquad_peaking(taps_buf, f, fs, 0.7, 3);
-  printf("taps : b0 %ld b1 %ld b2 %ld a1 %ld a2 %ld\n", taps_buf[0], taps_buf[1], taps_buf[2], taps_buf[3], taps_buf[4]);
-
   for (unsigned i = 0; i < 5; i++)
   {
-    //memset(&filter.coef[i][0], taps_buf[i], 8 * sizeof(int32_t));
-    filter.coef[i][0] = taps_buf[i];
+    fread(&taps_buf[i], sizeof(int32_t), 1, coeffs);
   }
+  fread(&lsh, sizeof(int32_t), 1, coeffs);
+  printf("%ld %ld %ld %ld %ld %d\n", taps_buf[0], taps_buf[1], taps_buf[2], taps_buf[3], taps_buf[4], lsh);
   
   for (unsigned i = 0; i < in_len; i++)
   {
     int32_t samp = 0, samp_out = 0;
     fread(&samp, sizeof(int32_t), 1, in);
     //printf("%ld ", samp);
-    //samp_out = adsp_apply_biquad(&filter, samp);
-    samp_out = filter_biquad_s32(&filter, samp);
+    samp_out = adsp_biquad(samp, taps_buf, state, lsh);
     //printf("%ld ", samp_out);
     fwrite(&samp_out, sizeof(int32_t), 1, out);
   }
--- test/biquad/test_biquad_c.py
+++ test/biquad/test_biquad_c.py
@@ -34,17 +34,9 @@ def get_sig(f = 1000, len = 0.25):
 
   return sig_fl, sig_int
 
-def get_c_wav(build = False, sim = True):
-  if build and xcommon_path != None:
-    make_cmd = f"XMOS_CMAKE_PATH={xcommon_path} make -C " + str(build_dir)
-    stdout = subprocess.check_output(make_cmd, cwd=build_dir, shell=True)
-    print("build msg:\n", stdout, "\n")
-  elif build and xcommon_path == None:
-    assert 0, "XMOS_CMAKE_PATH is not set"
-
-  if sim: app = "xsim"
-  else: app = "xrun"
-  run_cmd = app + " " + str(build_dir / "dummy_test.xe")
+def get_c_wav(sim = True):
+  app = "xsim" if sim else "xrun"
+  run_cmd = app + " " + str(build_dir / "biquad_test.xe")
   stdout = subprocess.check_output(run_cmd, cwd = build_dir, shell = True)
   print("run msg:\n", stdout)
 
@@ -52,20 +44,22 @@ def get_c_wav(build = False, sim = True):
   assert sig_bin.is_file(), f"Could not find output bin {sig_bin}"
   sig_int = np.fromfile(sig_bin, dtype=np.int32)
 
-  #sf.write(gen_dir / "sig_c.wav", sig_int, fs, "PCM_32")
   sf.write(gen_dir / "sig_c.wav", sig_int<<8, fs, "PCM_24")
 
 def run_py(sig_fl):
-  filt = bq.biquad_lowpass(fs, 1000, 0.7)
-  #filt = bq.biquad_peaking(fs, 1000, 1, 6)
+  #filt = bq.biquad_lowpass(fs, 1000, 0.7)
+  filt = bq.biquad_peaking(fs, 1000, 1, 6)
   output = np.zeros(sig_fl.size)
+  
   coeff_copy = np.array(filt.coeffs)
   coeff_copy = (coeff_copy * 2**30).astype(np.int32)
-  print(coeff_copy)
+  filt_info = np.append(coeff_copy.astype(np.int32), -filt.b_shift).astype(np.int32)
+  filt_info.tofile(build_dir / "coeffs.bin")
+
   for n in range(sig_fl.size):
     output[n] = filt.process_int(sig_fl[n])
   sf.write(gen_dir / "sig_py.wav", output, fs, "PCM_24")
 
 sig_fl, sig_int = get_sig()
-get_c_wav()
 run_py(sig_fl)
+get_c_wav()
