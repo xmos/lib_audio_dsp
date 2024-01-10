93eababd13cd8f760eaf95fbb33941b436faa8ba
Allan Skellett <allanskellett@xmos.com>
2024-01-10 16:28:56 -0500

    reduce changes from original test

--- test/biquad/test_biquad_c.py
+++ test/biquad/test_biquad_c.py
@@ -12,27 +12,28 @@ gen_dir = Path(__file__).parent / "autogen"
 fs = 48000
 Q_format = 27
 
+
 def float_to_qxx(arr_float, q = Q_format, dtype = np.int32):
   arr_int32 = np.clip((np.array(arr_float) * (2**q)), np.iinfo(dtype).min, np.iinfo(dtype).max).astype(dtype)
   return arr_int32
+
 
 def qxx_to_float(arr_int, q = Q_format):
   arr_float = np.array(arr_int).astype(np.float64) * (2 ** (-q))
   return arr_float
 
+
 def get_sig(len = 0.25):
   time = np.arange(0, len, 1 / fs)
-  sig_fl = np.round(0.8*np.sin(2*np.pi*997*time)*2**23)/2**23
-  # sig_fl = 0.5 * signal.chirp(time, 20, time[-1], 0.8 * fs / 2, "log", phi = -90)
+  sig_fl = 0.5 * signal.chirp(time, 20, time[-1], 0.8 * fs / 2, "log", phi = -90)
   sig_int = float_to_qxx(sig_fl)
+  sig_fl = qxx_to_float(sig_int)
   name = "sig_48k"
   sig_int.tofile(build_dir /  str(name + ".bin"))
   sf.write(gen_dir / str(name + ".wav"), sig_fl, int(fs), "PCM_24")
 
-
-  sig_fl, _ = sf.read(gen_dir / str(name + ".wav"))
-
   return sig_fl
+
 
 def get_c_wav(run = True, sim = True):
   app = "xsim" if sim else "xrun"
@@ -50,8 +51,9 @@ def get_c_wav(run = True, sim = True):
 
   sig_fl = qxx_to_float(sig_int)
 
-  sf.write(gen_dir / "sig_c.wav", sig_fl, fs, "PCM_32")
+  sf.write(gen_dir / "sig_c.wav", sig_fl, fs, "PCM_24")
   return sig_fl
+
 
 def run_py(filt, sig_fl):
   out_int = np.zeros(sig_fl.size)
@@ -60,16 +62,16 @@ def run_py(filt, sig_fl):
   for n in range(sig_fl.size):
     out_int[n] = filt.process_vpu(sig_fl[n])
 
-  sf.write(gen_dir / "sig_py_int.wav", out_int, fs, "PCM_32")
-  int2, _ = sf.read(gen_dir / "sig_py_int.wav")
+  sf.write(gen_dir / "sig_py_int.wav", out_int, fs, "PCM_24")
   filt.reset_state()
 
   for n in range(sig_fl.size):
     out_fl[n] = filt.process(sig_fl[n])
 
-  sf.write(gen_dir / "sig_py_flt.wav", out_fl, fs, "PCM_32")
+  sf.write(gen_dir / "sig_py_flt.wav", out_fl, fs, "PCM_24")
 
   return out_fl, out_int
+
 
 def compare(expected, actual, rtol, atol):
   for i in range(actual.size):
@@ -81,8 +83,8 @@ def compare(expected, actual, rtol, atol):
 
 @pytest.mark.parametrize("filter_type", [#"biquad_lowpass",
                                          #"biquad_highpass",
-                                         "biquad_notch",
-                                        #  "biquad_allpass"
+                                         #"biquad_notch",
+                                         "biquad_allpass"
                                          ])
 @pytest.mark.parametrize("f", [20, 
                                #200, 
@@ -92,7 +94,7 @@ def compare(expected, actual, rtol, atol):
 @pytest.mark.parametrize("q", [#0.1, 
                                #0.707, 
                                #5, 
-                               2
+                               10
                                ])
 def test_biquad_c(filter_type, f, q):
   print("")
@@ -104,9 +106,9 @@ def test_biquad_c(filter_type, f, q):
   filt_info = np.append(filt.int_coeffs, filt.b_shift)
   filt_info.tofile(build_dir / "coeffs.bin")
 
-  sig_fl = get_sig(1)
+  sig_fl = get_sig()
   out_py_fl, out_py_int = run_py(filt, sig_fl)
-  out_c = get_c_wav(run=True)
+  out_c = get_c_wav()
 
   np.testing.assert_allclose(out_c, out_py_int, rtol=0, atol=0)
 
