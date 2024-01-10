4e144ab2ee35fc758dc9387c06a7237e708d3625
Allan Skellett <allanskellett@xmos.com>
2024-01-10 16:15:38 -0500

    passing c vs python test

--- test/biquad/test_biquad_c.py
+++ test/biquad/test_biquad_c.py
@@ -22,12 +22,15 @@ def qxx_to_float(arr_int, q = Q_format):
 
 def get_sig(len = 0.25):
   time = np.arange(0, len, 1 / fs)
-
-  sig_fl = 0.5 * signal.chirp(time, 20, time[-1], 0.8 * fs / 2, "log", phi = -90)
+  sig_fl = np.round(0.8*np.sin(2*np.pi*997*time)*2**23)/2**23
+  # sig_fl = 0.5 * signal.chirp(time, 20, time[-1], 0.8 * fs / 2, "log", phi = -90)
   sig_int = float_to_qxx(sig_fl)
   name = "sig_48k"
   sig_int.tofile(build_dir /  str(name + ".bin"))
   sf.write(gen_dir / str(name + ".wav"), sig_fl, int(fs), "PCM_24")
+
+
+  sig_fl, _ = sf.read(gen_dir / str(name + ".wav"))
 
   return sig_fl
 
@@ -37,6 +40,9 @@ def get_c_wav(run = True, sim = True):
   if run:
     stdout = subprocess.check_output(run_cmd, cwd = build_dir, shell = True)
     #print("run msg:\n", stdout)
+  else:
+    sig_fl, _ = sf.read(gen_dir / "sig_c.wav")
+    return sig_fl
 
   sig_bin = build_dir / "sig_out.bin"
   assert sig_bin.is_file(), f"Could not find output bin {sig_bin}"
@@ -44,7 +50,7 @@ def get_c_wav(run = True, sim = True):
 
   sig_fl = qxx_to_float(sig_int)
 
-  sf.write(gen_dir / "sig_c.wav", sig_fl, fs, "PCM_24")
+  sf.write(gen_dir / "sig_c.wav", sig_fl, fs, "PCM_32")
   return sig_fl
 
 def run_py(filt, sig_fl):
@@ -52,14 +58,17 @@ def run_py(filt, sig_fl):
   out_fl = np.zeros(sig_fl.size)
   
   for n in range(sig_fl.size):
-    out_int[n] = filt.process_int(sig_fl[n])
+    out_int[n] = filt.process_vpu(sig_fl[n])
 
+  sf.write(gen_dir / "sig_py_int.wav", out_int, fs, "PCM_32")
+  int2, _ = sf.read(gen_dir / "sig_py_int.wav")
   filt.reset_state()
 
   for n in range(sig_fl.size):
     out_fl[n] = filt.process(sig_fl[n])
 
-  sf.write(gen_dir / "sig_py.wav", out_fl, fs, "PCM_24")
+  sf.write(gen_dir / "sig_py_flt.wav", out_fl, fs, "PCM_32")
+
   return out_fl, out_int
 
 def compare(expected, actual, rtol, atol):
@@ -72,8 +81,8 @@ def compare(expected, actual, rtol, atol):
 
 @pytest.mark.parametrize("filter_type", [#"biquad_lowpass",
                                          #"biquad_highpass",
-                                         #"biquad_notch",
-                                         "biquad_allpass"
+                                         "biquad_notch",
+                                        #  "biquad_allpass"
                                          ])
 @pytest.mark.parametrize("f", [20, 
                                #200, 
@@ -83,7 +92,7 @@ def compare(expected, actual, rtol, atol):
 @pytest.mark.parametrize("q", [#0.1, 
                                #0.707, 
                                #5, 
-                               10
+                               2
                                ])
 def test_biquad_c(filter_type, f, q):
   print("")
@@ -92,20 +101,18 @@ def test_biquad_c(filter_type, f, q):
 
   filter_handle = getattr(bq, filter_type)
   filt = filter_handle(fs, f, q)
-  filt_info = np.append(filt.int_coeffs, filt.b_shift).astype(np.int32)
+  filt_info = np.append(filt.int_coeffs, filt.b_shift)
   filt_info.tofile(build_dir / "coeffs.bin")
 
-  sig_fl = get_sig()
+  sig_fl = get_sig(1)
   out_py_fl, out_py_int = run_py(filt, sig_fl)
-  out_c = get_c_wav()
+  out_c = get_c_wav(run=True)
 
-  #np.testing.assert_allclose(out_c, out_py_int, rtol=1e-5, atol=7.4e-7, verbose=True)
-  np.testing.assert_allclose(out_c, out_py_int, rtol=8e-5, atol=2.853e-4, verbose=True)
-  #np.testing.assert_allclose(out_c, out_py_fl, rtol=1e-5, atol=6.9e-7, verbose=True)
-  np.testing.assert_allclose(out_c, out_py_fl, rtol=8e-5, atol=2.874e-4, verbose=True)
-  #compare(out_py_int, out_c, 8e-5, 2.853e-4)
-  #compare(out_py_fl, out_c, 8e-5, 2.874e-4)
+  np.testing.assert_allclose(out_c, out_py_int, rtol=0, atol=0)
 
   #for i in range(out_c.size):
   #  if not np.isclose(out_c[i], out_py_int[i], rtol=1e-5, atol=7.1e-7):
   #    print(f"C {out_c[i]} PY {out_py_int[i]} abs diff {out_c[i] - out_py_int[i]} rel diff {1 - out_c[i]/out_py_int[i]}")
+
+if __name__ =="__main__":
+  test_biquad_c("biquad_notch", 20, 2)
