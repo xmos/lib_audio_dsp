c95bc01b857a17fbd1a47802a6faf5cd4ab1e2d8
Allan Skellett <allanskellett@xmos.com>
2024-01-18 12:55:24 -0500

    fewer faster python biquad tests

--- test/biquad/test_biquad_python.py
+++ test/biquad/test_biquad_python.py
@@ -6,7 +6,7 @@ import audio_dsp.dsp.utils as utils
 
 
 def chirp_filter_test(filter: bq.biquad, fs):
-    length = 0.5
+    length = 0.05
     signal = gen.log_chirp(fs, length, 0.5)
 
     output_int = np.zeros(len(signal))
@@ -37,10 +37,10 @@ def chirp_filter_test(filter: bq.biquad, fs):
                                          "biquad_constant_q",
                                          "biquad_lowshelf",
                                          "biquad_highshelf",])
-@pytest.mark.parametrize("f", [20, 100, 200, 1000, 2000, 10000, 20000])
-@pytest.mark.parametrize("q", [0.1, 0.5, 0.707, 1, 1.41, 2, 5, 10])
-@pytest.mark.parametrize("gain", [-12, -10, -5, 0, 5, 10, 12])
-@pytest.mark.parametrize("fs", [16000, 24000, 44100, 48000, 88200, 96000, 192000])
+@pytest.mark.parametrize("f", [20, 100, 1000, 10000, 20000])
+@pytest.mark.parametrize("q", [0.1, 0.5, 1, 2, 10])
+@pytest.mark.parametrize("gain", [-12, -6, 0, 6, 12])
+@pytest.mark.parametrize("fs", [16000, 44100, 48000, 88200, 96000, 192000])
 def test_high_gain(filter_type, f, q, gain, fs):
     filter_handle = getattr(bq, "make_%s" % filter_type)
     filter = bq.biquad(filter_handle(fs, np.min([f, fs/2*0.95]), q, gain), fs, b_shift=2)
@@ -51,9 +51,9 @@ def test_high_gain(filter_type, f, q, gain, fs):
                                          "biquad_highpass",
                                          "biquad_notch",
                                          "biquad_allpass"])
-@pytest.mark.parametrize("f", [20, 100, 200, 1000, 2000, 10000, 20000])
-@pytest.mark.parametrize("q", [0.1, 0.5, 0.707, 1, 1.41, 2, 5, 10])
-@pytest.mark.parametrize("fs", [16000, 24000, 44100, 48000, 88200, 96000, 192000])
+@pytest.mark.parametrize("f", [20, 100, 1000, 10000, 20000])
+@pytest.mark.parametrize("q", [0.1, 0.5, 1, 2, 5, 10])
+@pytest.mark.parametrize("fs", [16000, 44100, 48000, 88200, 96000, 192000])
 def test_xpass_filters(filter_type, f, q, fs):
 
     if f < fs*5e-4 and filter_type == "biquad_lowpass":
@@ -66,9 +66,9 @@ def test_xpass_filters(filter_type, f, q, fs):
 
 @pytest.mark.parametrize("filter_type", ["biquad_bandpass",
                                          "biquad_bandstop",])
-@pytest.mark.parametrize("f", [100, 200, 1000, 2000, 10000, 20000])
-@pytest.mark.parametrize("q", [0.1, 0.5, 0.707, 1, 1.41, 2, 5, 10])
-@pytest.mark.parametrize("fs", [16000, 24000, 44100, 48000, 88200, 96000, 192000])
+@pytest.mark.parametrize("f", [100, 1000, 10000, 20000])
+@pytest.mark.parametrize("q", [0.1, 0.5, 1, 2, 5, 10])
+@pytest.mark.parametrize("fs", [16000, 44100, 48000, 88200, 96000, 192000])
 def test_bandx_filters(filter_type, f, q, fs):
     filter_handle = getattr(bq, "make_%s" % filter_type)
     f = np.min([f, fs/2*0.95])
@@ -82,11 +82,12 @@ def test_bandx_filters(filter_type, f, q, fs):
     chirp_filter_test(filter, fs)
 
 
-@pytest.mark.parametrize("f0,", [20, 50, 100, 200, 500])
-@pytest.mark.parametrize("fp_ratio", [0.4, 0.5, 1, 2, 4])
-@pytest.mark.parametrize("q0", [0.5, 0.707, 1, 1.41, 2])
-@pytest.mark.parametrize("qp", [0.5, 0.707, 1, 1.41, 2])
-@pytest.mark.parametrize("fs", [16000, 24000, 44100, 48000, 88200, 96000, 192000])
+@pytest.mark.parametrize("f0,", [20, 50, 100, 200])
+@pytest.mark.parametrize("fp_ratio", [0.4, 1, 4])
+@pytest.mark.parametrize("q0, qp", [(0.5, 2),
+                                    (2, 0.5),
+                                    (0.707, 0.707)])
+@pytest.mark.parametrize("fs", [16000, 44100, 48000, 88200, 96000, 192000])
 def test_linkwitz_filters(f0, fp_ratio, q0, qp, fs):
 
     if fs > 100000 and f0 < 50 and fp_ratio < 1:
