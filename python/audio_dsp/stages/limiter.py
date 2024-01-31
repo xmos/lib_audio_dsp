73725ccf8c3f999620386d5cde46d87d84b4e820
Shuchita Khare <shuchitak@xmos.com>
2024-01-31 11:24:43 +0000

    review comments

--- python/audio_dsp/stages/limiter.py
+++ python/audio_dsp/stages/limiter.py
@@ -8,11 +8,10 @@ class LimiterPeak(Stage):
         super().__init__(config=find_config("limiter_peak"), **kwargs)
         self.create_outputs(self.n_in)
 
-        fs = 48000
         threshold = 0
         at = 0.01
         rt = 0.2
-        self.lt = drc.limiter_peak(fs, threshold, at, rt)
+        self.lt = drc.limiter_peak(self.fs, threshold, at, rt)
 
         self.set_control_field_cb("attack_alpha",
                                   lambda: self.lt.attack_alpha_uq30)
@@ -23,7 +22,7 @@ class LimiterPeak(Stage):
 
     def process(self, in_channels):
         """
-        Run Biquad on the input channels and return the output
+        Run the limiter on the input channels and return the output
 
         Args:
             in_channels: list of numpy arrays
