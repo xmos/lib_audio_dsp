82e45fadd7961834c9769fceddfcad71f2655a44
Allan Skellett <allanskellett@xmos.com>
2024-02-12 16:44:16 -0500

    add process_xcore functions for signal chain components

--- python/audio_dsp/dsp/signal_chain.py
+++ python/audio_dsp/dsp/signal_chain.py
@@ -11,12 +11,24 @@ class mixer(dspg.dsp_block):
         self.num_channels = num_channels
         self.gain_db = gain_db
         self.gain = utils.db2gain(gain_db)
+        self.gain_int = utils.int32(self.gain * 2**30)
 
     def process(self, sample_list):
         scaled_samples = np.array(sample_list)*self.gain
         y = np.sum(scaled_samples)
 
         return y
+
+    def process_xcore(self, sample_list):
+        y = 0
+        for sample in sample_list:
+            sample_int = utils.int32(round(sample * 2**self.Q_sig))
+            scaled_sample = utils.vpu_mult(sample_int, self.gain_int)
+            y = utils.int32(y + scaled_sample)
+
+        y_flt = (float(y)*2**-self.Q_sig)
+
+        return y_flt
 
     def freq_response(self, nfft=512):
         # flat response scaled by gain
@@ -38,6 +50,16 @@ class subtractor(dspg.dsp_block):
 
         return y
 
+    def process_xcore(self, sample_list):
+        sample_int_0 = utils.int32(round(sample_list[0] * 2**self.Q_sig))
+        sample_int_1 = utils.int32(round(sample_list[1] * 2**self.Q_sig))
+
+        y = utils.int32(sample_int_0 - sample_int_1)
+
+        y_flt = (float(y)*2**-self.Q_sig)
+
+        return y_flt
+
 
 class fixed_gain(dspg.dsp_block):
     # multiply every sample by a fixed gain value
@@ -45,10 +67,19 @@ class fixed_gain(dspg.dsp_block):
         super().__init__(Q_sig)
         self.gain_db = gain_db
         self.gain = utils.db2gain(gain_db)
+        self.gain_int = utils.int32(self.gain * 2**30)
 
     def process(self, sample):
         y = sample*self.gain
         return y
+
+    def process_xcore(self, sample):
+        sample_int = utils.int32(round(sample * 2**self.Q_sig))
+        y = utils.vpu_mult(sample_int, self.gain_int)
+
+        y_flt = (float(y)*2**-self.Q_sig)
+
+        return y_flt
 
     def freq_response(self, nfft=512):
         # flat response scaled by gain
@@ -62,6 +93,7 @@ class volume_control(fixed_gain):
     def set_gain(self, gain_db):
         self.gain_db = gain_db
         self.gain = utils.db2gain(gain_db)
+        self.gain_int = utils.int32(self.gain * 2**30)
 
 
 class switch(dspg.dsp_block):
@@ -73,6 +105,9 @@ class switch(dspg.dsp_block):
     def process(self, sample_list):
         y = sample_list[self.switch_position]
         return y
+
+    def process_xcore(self, sample_list):
+        return self.process(sample_list)
 
     def move_switch(self, position):
         self.switch_position = position
