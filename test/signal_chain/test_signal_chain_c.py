6050c50c6e379b336743d5f0c5d8368f3d1d9733
uvvpavel <paveluvarov@xmos.com>
2024-02-21 16:05:40 +0000

    actually adding new files

---
+++ test/signal_chain/test_signal_chain_c.py
@@ -0,0 +1,132 @@
+# Copyright 2024 XMOS LIMITED.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
+import numpy as np
+import soundfile as sf
+from pathlib import Path
+import shutil
+import subprocess
+import audio_dsp.dsp.signal_chain as sc
+from audio_dsp.dsp.generic import Q_SIG
+import audio_dsp.dsp.signal_gen as gen
+import pytest
+import random
+
+bin_dir = Path(__file__).parent / "bin"
+gen_dir = Path(__file__).parent / "autogen"
+
+fs = 48000
+
+
+def float_to_qxx(arr_float, q = Q_SIG, dtype = np.int32):
+  arr_int32 = np.clip((np.array(arr_float) * (2**q)), np.iinfo(dtype).min, np.iinfo(dtype).max).astype(dtype)
+  return arr_int32
+
+
+def qxx_to_float(arr_int, q = Q_SIG):
+  arr_float = np.array(arr_int).astype(np.float64) * (2 ** (-q))
+  return arr_float
+
+
+def get_sig(len=0.05):
+  sig_fl = []
+  sig_fl.append(gen.sin(fs, len, 997, 0.8))
+  sig_fl.append(gen.sin(fs, len, 100, 0.8))
+  sig_fl = np.stack(sig_fl, axis=0)
+  sig_int = float_to_qxx(sig_fl)
+
+  name = "sig_48k"
+  sig_int[0].tofile(bin_dir /  f"{name}.bin")
+  sf.write(gen_dir / f"{name}.wav", sig_fl[0], int(fs), "PCM_24")
+
+  name = "sig1_48k"
+  sig_int[1].tofile(bin_dir /  f"{name}.bin")
+  sf.write(gen_dir / f"{name}.wav", sig_fl[1], int(fs), "PCM_24")
+
+  return sig_fl
+
+def get_c_wav(dir_name, comp_name, sim = True):
+  app = "xsim" if sim else "xrun --io"
+  run_cmd = app + " " + str(bin_dir / f"{comp_name}_test.xe")
+  stdout = subprocess.check_output(run_cmd, cwd = dir_name, shell = True)
+  #print("run msg:\n", stdout)
+
+  sig_bin = dir_name / "sig_out.bin"
+  assert sig_bin.is_file(), f"Could not find output bin {sig_bin}"
+  sig_int = np.fromfile(sig_bin, dtype=np.int32)
+
+  sig_fl = qxx_to_float(sig_int)
+  sf.write(gen_dir / "sig_c.wav", sig_fl, fs, "PCM_24")
+  return sig_fl
+
+
+def run_py(filt, sig_fl):
+  out_int = np.zeros(sig_fl.size)
+  out_fl = np.zeros(sig_fl.size)
+  
+  for n in range(sig_fl.size):
+    out_int[n] = filt.process_xcore(sig_fl[n])
+
+  sf.write(gen_dir / "sig_py_int.wav", out_int, fs, "PCM_24")
+
+  for n in range(sig_fl.size):
+    out_fl[n] = filt.process(sig_fl[n])
+
+  sf.write(gen_dir / "sig_py_flt.wav", out_fl, fs, "PCM_24")
+
+  return out_fl, out_int
+
+def write_gain(test_dir, gain):
+  all_filt_info = np.empty(0, dtype=np.int32)
+  all_filt_info = np.append(all_filt_info, gain)
+  all_filt_info.tofile(test_dir / "gain.bin")
+
+def single_test(filt, tname, sig_fl):
+  test_dir = bin_dir / tname
+  test_dir.mkdir(exist_ok = True, parents = True)
+
+  write_gain(test_dir, filt.gain_int)
+
+  out_py_fl, out_py_int = run_py(filt, sig_fl)
+  out_c = get_c_wav(test_dir, "fixed_gain")
+  shutil.rmtree(test_dir)
+
+  np.testing.assert_allclose(out_c, out_py_int, rtol=0, atol=0)
+
+@pytest.fixture(scope="module")
+def in_signal():
+  bin_dir.mkdir(exist_ok=True, parents=True)
+  gen_dir.mkdir(exist_ok=True, parents=True)
+  return get_sig()
+
+@pytest.mark.parametrize("gain_dB", [-10, 0, 24])
+def test_gains_c(in_signal, gain_dB):
+  filt = sc.fixed_gain(fs, 1, gain_dB)
+  filter_name = f"fixed_gain_{gain_dB}"
+
+  single_test(filt, filter_name, in_signal[0])
+
+def test_subtractor_c(in_signal):
+  filt = sc.subtractor(fs)
+  test_dir = bin_dir / "subtractor"
+  test_dir.mkdir(exist_ok = True, parents = True)
+
+  out_py = np.zeros(in_signal.shape[1])
+
+  for n in range(in_signal.shape[1]):
+    out_py[n] = filt.process_xcore(in_signal[:, n])
+  
+  sf.write(gen_dir / "sig_py_int.wav", out_py, fs, "PCM_24")
+
+  out_c = get_c_wav(test_dir, "subtractor")
+  shutil.rmtree(test_dir)
+
+  np.testing.assert_allclose(out_c, out_py, rtol=0, atol=0)
+
+
+if __name__ =="__main__":
+  bin_dir.mkdir(exist_ok=True, parents=True)
+  gen_dir.mkdir(exist_ok=True, parents=True)
+  sig_fl = get_sig()
+  
+  #test_gains_c(sig_fl, -6)
+  test_subtractor_c(sig_fl)
