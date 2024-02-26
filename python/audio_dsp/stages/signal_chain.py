9d1607f4bc74bd484711d4e71a0968fb01499c0d
Allan Skellett <allanskellett@xmos.com>
2024-02-26 15:40:27 -0500

    add signal chain components to Stage

--- python/audio_dsp/stages/signal_chain.py
+++ python/audio_dsp/stages/signal_chain.py
@@ -3,7 +3,7 @@
 
 from ..design.stage import Stage, find_config, StageOutput
 from operator import itemgetter
-
+import audio_dsp.dsp.signal_chain as sc
 
 class Bypass(Stage):
     """
@@ -43,3 +43,102 @@ class Fork(Stage):
     def get_frequency_response(self, nfft=512):
         # not sure what this looks like!
         raise NotImplementedError
+
+
+class Mixer(Stage):
+    """
+    Mixes the input signals together. The mixer can be used to add signals
+    together, or to attenuate the input signals.
+
+    Attributes
+    ----------
+    gain_db : float
+        The gain of the mixer in dB.
+    """
+
+    def __init__(self, gain_db=-6, **kwargs):
+        super().__init__(config=find_config("mixer"), **kwargs)
+        self.fs = int(self.fs)
+        self.gain_db = gain_db
+        self.create_outputs(self.n_in)
+        self.dsp_block = sc.mixer(self.fs, self.n_in, gain_db)
+        self.set_control_field_cb(
+            "gain", lambda: [i for i in self.get_fixed_point_coeffs()]
+        )
+
+
+class Adder(Stage):
+    """
+    Add the input signals together. The adder can be used to add signals
+    together, or to attenuate the input signals.
+
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(config=find_config("adder"), **kwargs)
+        self.fs = int(self.fs)
+        self.create_outputs(self.n_in)
+        self.dsp_block = sc.adder(self.fs, self.n_in)
+
+
+class Subtractor(Stage):
+    """
+    Subtract the second input from the first. The subtractor can be used to
+    subtract signals from each other.
+
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(config=find_config("subtractor"), **kwargs)
+        self.fs = int(self.fs)
+        self.create_outputs(self.n_in)
+        self.dsp_block = sc.subtractor(self.fs, self.n_in)
+
+
+class Fixed_gain(Stage):
+    """
+    Multiply the input by a fixed gain. The gain is set at the time of
+    construction and cannot be changed.
+
+    """
+
+    def __init__(self, gain_db=-6, **kwargs):
+        super().__init__(config=find_config("fixed_gain"), **kwargs)
+        self.fs = int(self.fs)
+        self.gain_db = gain_db
+        self.create_outputs(self.n_in)
+        self.dsp_block = sc.fixed_gain(self.fs, self.n_in, gain_db)
+
+
+class Volume_control(Stage):
+    """
+    Multiply the input by a gain. The gain can be changed at runtime.
+
+    """
+
+    def __init__(self, gain_db=-6, **kwargs):
+        super().__init__(config=find_config("volume_control"), **kwargs)
+        self.fs = int(self.fs)
+        self.gain_db = gain_db
+        self.create_outputs(self.n_in)
+        self.dsp_block = sc.volume_control(self.fs, self.n_in, gain_db)
+        self.set_control_field_cb(
+            "gain", lambda: self.dsp_block.set_gain
+        )
+
+class Switch(Stage):
+    """
+    Switch the input to one of the outputs. The switch can be used to
+    select between different signals.
+
+    """
+
+    def __init__(self, index=0, **kwargs):
+        super().__init__(config=find_config("switch"), **kwargs)
+        self.fs = int(self.fs)
+        self.index = index
+        self.create_outputs(self.n_in)
+        self.dsp_block = sc.switch(self.fs, self.n_in)
+        self.set_control_field_cb(
+            "position", lambda: self.dsp_block.move_switch
+        )
