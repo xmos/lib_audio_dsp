4c8c66c0fcbbbdceb32177e50dae5add50ac7f0b
Allan Skellett <allanskellett@xmos.com>
2024-03-15 16:29:03 -0400

    use redundant imports instead of __all__ in __init__

--- python/audio_dsp/dsp/drc/__init__.py
+++ python/audio_dsp/dsp/drc/__init__.py
@@ -1,32 +1,18 @@
 from audio_dsp.dsp.drc.drc import (
-    compressor_limiter_base,
-    envelope_detector_peak,
-    envelope_detector_rms,
-    limiter_peak,
-    limiter_rms,
-    compressor_rms,
-    noise_gate,
+    compressor_limiter_base as compressor_limiter_base,
+    envelope_detector_peak as envelope_detector_peak,
+    envelope_detector_rms as envelope_detector_rms,
+    limiter_peak as limiter_peak,
+    limiter_rms as limiter_rms,
+    compressor_rms as compressor_rms,
+    noise_gate as noise_gate,
 )
 from audio_dsp.dsp.drc.stereo_compressor_limiter import (
-    limiter_peak_stereo,
-    compressor_rms_stereo,
+    limiter_peak_stereo as limiter_peak_stereo,
+    compressor_rms_stereo as compressor_rms_stereo,
 )
 
 from audio_dsp.dsp.drc.sidechain import (
-    compressor_rms_sidechain_mono,
-    compressor_rms_sidechain_stereo,
+    compressor_rms_sidechain_mono as compressor_rms_sidechain_mono,
+    compressor_rms_sidechain_stereo as compressor_rms_sidechain_stereo,
 )
-
-__all__ = [
-    compressor_limiter_base,
-    envelope_detector_peak,
-    envelope_detector_rms,
-    limiter_peak,
-    limiter_rms,
-    compressor_rms,
-    noise_gate,
-    limiter_peak_stereo,
-    compressor_rms_stereo,
-    compressor_rms_sidechain_mono,
-    compressor_rms_sidechain_stereo,
-]
