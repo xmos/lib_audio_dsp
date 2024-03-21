f1f00131808fba88e895134bebbfd3aee95bc3e0
Allan Skellett <allanskellett@xmos.com>
2024-03-21 16:13:23 -0400

    int32 implementation

--- python/audio_dsp/dsp/reverb.py
+++ python/audio_dsp/dsp/reverb.py
@@ -6,11 +6,24 @@ import numpy as np
 
 import audio_dsp.dsp.utils as utils
 
+Q_VERB = 31
+
+
+def apply_gain_xcore(sample, gain):
+    """Apply the gain to a sample usign fixed-point math, assumes that gain is in Q_alpha format"""
+    acc = 1 << (Q_VERB - 1)
+    acc += sample * gain
+    y = utils.int32_mult_sat_extract(acc, 1, Q_VERB)
+    return y
+
+
 class allpass_fv(dspg.dsp_block):
     def __init__(self, delay, feedback_gain):
         self.delay = delay
         self._feedback = feedback_gain
+        self.feedback_int = utils.int32(self._feedback * 2**Q_VERB)
         self.buffer = np.zeros(self.delay)
+        self.buffer_int = [0] * self.delay
         self.buffer_idx = 0
         
     def process(self, sample):
@@ -26,16 +39,39 @@ class allpass_fv(dspg.dsp_block):
 
         return output
 
+    def process_xcore(self, sample_int):
+        assert isinstance(sample_int, int), "Input sample must be an integer"
+
+        buff_out = self.buffer_int[self.buffer_idx]
+
+        output = utils.int32(-sample_int + buff_out)
+        self.buffer_int[self.buffer_idx] = sample_int 
+        self.buffer_int[self.buffer_idx] += apply_gain_xcore(buff_out, self.feedback_int)
+        # check for overflow
+        utils.int32(self.buffer_int[self.buffer_idx])
+
+        # move buffer head
+        self.buffer_idx += 1
+        if self.buffer_idx >= self.delay:
+            self.buffer_idx = 0
+
+        return output
+
 
 class comb_fv(dspg.dsp_block):
     def __init__(self, delay, feedback_gain, damping):
         self._delay = delay
         self._feedback = feedback_gain
+        self.feedback_int = utils.int32(self._feedback * 2**Q_VERB)
         self._buffer = np.zeros(self._delay)
+        self.buffer_int = [0] * self._delay
         self.buffer_idx = 0
         self.filterstore = 0
+        self.filterstore_int = 0
         self.damp1 = damping
         self.damp2 = 1 - self.damp1
+        self.damp1_int = utils.int32(self.damp1 * 2**Q_VERB)
+        self.damp2_int = utils.int32(2**31 - 1 - self.damp1_int)
         
     def process(self, sample):
 
@@ -51,14 +87,35 @@ class comb_fv(dspg.dsp_block):
 
         return output
 
+    def process_xcore(self, sample_int):
+        assert isinstance(sample_int, int), "Input sample must be an integer"
+
+        output = self.buffer_int[self.buffer_idx]
+
+        self.filterstore_int = apply_gain_xcore(self.filterstore_int, self.damp1_int)
+        self.filterstore_int += apply_gain_xcore(output, self.damp2_int)
+        utils.int32(self.filterstore_int)
+
+        self.buffer_int[self.buffer_idx] = sample_int 
+        self.buffer_int[self.buffer_idx] += apply_gain_xcore(self.filterstore_int, self.feedback_int)
+        utils.int32(self.buffer_int[self.buffer_idx])
+        
+        self.buffer_idx += 1
+        if self.buffer_idx >= self._delay:
+            self.buffer_idx = 0
+
+        return output
 
 class freeverb(dspg.dsp_block):
-    def __init__(self, fs, n_chans, room_size=0.5, decay=0.5, damping=0.4, wet_gain_db=-1, dry_gain_db=-1, Q_sig=dspg.Q_SIG,):
+    def __init__(self, fs, n_chans, max_room_size=1, room_size=0.5, decay=0.5, damping=0.4, wet_gain_db=-1, dry_gain_db=-1, Q_sig=dspg.Q_SIG,):
         """A room reverb effect based on Freeverb by Jezar at
         Dreampoint.
 
         Parameters
         ----------
+        max_room_size : float, optional
+            sets the maximum size of the delay buffers, can only be set
+            at initialisation
         room_size : float, optional
             how big the room is, sets delay line lengths. Likely between
             0 and 1, unless you have a huge room
@@ -72,11 +129,19 @@ class freeverb(dspg.dsp_block):
             dry signal gain
         """
         super().__init__(fs, 1, Q_sig)
+
         self.damping = damping
         self.feedback = decay*0.28 + 0.7  # avoids too much or too little feedback
         self.wet = utils.db2gain(wet_gain_db)
         self.dry = utils.db2gain(dry_gain_db)
-        self.gain = 0.015  # pregain going into the reverb
+
+        self.wet_int = utils.int32(self.wet * 2**Q_VERB)
+        self.dry_int = utils.int32(self.dry * 2**Q_VERB)
+
+        # pregain going into the reverb
+        self.gain = 0.015  
+        self.gain_int = utils.int32(self.gain * 2**Q_VERB)
+
         self.room_size = room_size
 
         # the magic freeverb delay line lengths are for 44.1kHz, so
@@ -112,9 +177,9 @@ class freeverb(dspg.dsp_block):
     def process(self, sample):
 
         output = 0
-        input = sample*self.gain
+        reverb_input = sample*self.gain
         for cb in self.combs:
-            output += cb.process(input)
+            output += cb.process(reverb_input)
 
         for ap in self.allpasses:
             output = ap.process(output)
@@ -122,18 +187,42 @@ class freeverb(dspg.dsp_block):
         output = output*self.wet + sample*self.dry
         return output
 
+    def process_xcore(self, sample):
+
+        sample_int = utils.int32(round(sample * 2**self.Q_sig))
+
+        output = 0
+
+        reverb_input = apply_gain_xcore(sample_int, self.gain_int)
+
+        for cb in self.combs:
+            output += cb.process_xcore(reverb_input)
+
+        for ap in self.allpasses:
+            output = ap.process_xcore(output)
+
+        output = apply_gain_xcore(output, self.wet_int)
+        output += apply_gain_xcore(sample_int, self.dry_int)
+        utils.int32(output)
+
+        return (float(output) * 2**-self.Q_sig)
 
 if __name__ == "__main__":
-    hydra_audio_path = os.environ['hydra_audio_PATH']
-    filepath = Path(hydra_audio_path, 'acoustic_team_test_audio',
-                    'speech', "010_male_female_single-talk_seq.wav")
+    # hydra_audio_path = os.environ['hydra_audio_PATH']
+    # filepath = Path(hydra_audio_path, 'acoustic_team_test_audio',
+    #                 'speech', "010_male_female_single-talk_seq.wav")
+    filepath = Path(r"C:\Users\allanskellett\Documents\014_ACM\T-REC-P.501-202005-I!!SOFT-ZST-E\Speech signals\Test Signals Clause 7\Test_Signals_Clause 7\Speech Test Signals Clause 7.3 & 7.4\English_FB_clause_7.3\FB_male_female_single-talk_seq.wav")
     sig, fs = sf.read(filepath)
 
-    reverb = freeverb()
+    sig = sig[:fs*5]
+    sig = sig/np.max(np.abs(sig))
+    sig = sig* (2**31 - 1)/(2**31)
+
+    reverb = freeverb(fs, 1, room_size=1, decay=1, damping=0.0, Q_sig=31)
     print(reverb.get_buffer_lens())
     
     output = np.zeros_like(sig)
     for n in range(len(sig)):
-        output[n] = reverb.process(sig[n])
+        output[n] = reverb.process_xcore(sig[n])
     
     sf.write('reverb_out.wav', output, fs)
