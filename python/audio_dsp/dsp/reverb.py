498e65130b894cb49db8a525c2bdf3d111925c87
Allan Skellett <allanskellett@xmos.com>
2024-03-22 15:41:58 -0400

    add max buffer size

--- python/audio_dsp/dsp/reverb.py
+++ python/audio_dsp/dsp/reverb.py
@@ -18,91 +18,114 @@ def apply_gain_xcore(sample, gain):
 
 
 class allpass_fv(dspg.dsp_block):
-    def __init__(self, delay, feedback_gain):
-        self.delay = delay
-        self._feedback = feedback_gain
-        self.feedback_int = utils.int32(self._feedback * 2**Q_VERB)
-        self.buffer = np.zeros(self.delay)
-        self.buffer_int = [0] * self.delay
-        self.buffer_idx = 0
-        
+    def __init__(self, max_delay, starting_delay, feedback_gain):
+        # max delay cannot be changed, or you'll overflow the buffer
+        self._max_delay = max_delay
+        self._buffer = np.zeros(self._max_delay)
+        self._buffer_int = [0] * self._max_delay
+
+        self.delay = starting_delay
+        self.feedback = feedback_gain
+        self.feedback_int = utils.int32(self.feedback * 2**Q_VERB)
+        self._buffer_idx = 0
+
+    def set_delay(self, delay):
+        if delay < self._max_delay:
+            self.delay = delay
+        else:
+            self.delay = self._max_delay
+            Warning("Delay cannot be greater than max delay, setting to max delay")
+        return
+
     def process(self, sample):
 
-        buff_out = self.buffer[self.buffer_idx]
+        buff_out = self._buffer[self._buffer_idx]
 
         output = -sample + buff_out
-        self.buffer[self.buffer_idx] = sample + (buff_out*self._feedback)
+        self._buffer[self.buffer_idx] = sample + (buff_out*self.feedback)
         
-        self.buffer_idx += 1
-        if self.buffer_idx >= self.delay:
-            self.buffer_idx = 0
+        self._buffer_idx += 1
+        if self._buffer_idx >= self.delay:
+            self._buffer_idx = 0
 
         return output
 
     def process_xcore(self, sample_int):
         assert isinstance(sample_int, int), "Input sample must be an integer"
 
-        buff_out = self.buffer_int[self.buffer_idx]
+        buff_out = self._buffer_int[self._buffer_idx]
 
         output = utils.int32(-sample_int + buff_out)
-        self.buffer_int[self.buffer_idx] = sample_int 
-        self.buffer_int[self.buffer_idx] += apply_gain_xcore(buff_out, self.feedback_int)
+        self._buffer_int[self._buffer_idx] = sample_int 
+        self._buffer_int[self._buffer_idx] += apply_gain_xcore(buff_out, self.feedback_int)
         # check for overflow
-        utils.int32(self.buffer_int[self.buffer_idx])
+        utils.int32(self._buffer_int[self._buffer_idx])
 
         # move buffer head
-        self.buffer_idx += 1
-        if self.buffer_idx >= self.delay:
-            self.buffer_idx = 0
+        self._buffer_idx += 1
+        if self._buffer_idx >= self.delay:
+            self._buffer_idx = 0
 
         return output
 
 
 class comb_fv(dspg.dsp_block):
-    def __init__(self, delay, feedback_gain, damping):
-        self._delay = delay
-        self._feedback = feedback_gain
-        self.feedback_int = utils.int32(self._feedback * 2**Q_VERB)
-        self._buffer = np.zeros(self._delay)
-        self.buffer_int = [0] * self._delay
-        self.buffer_idx = 0
-        self.filterstore = 0
-        self.filterstore_int = 0
+    def __init__(self, max_delay, starting_delay, feedback_gain, damping):
+        # max delay cannot be changed, or you'll overflow the buffer
+        self._max_delay = max_delay
+        self._buffer = np.zeros(self._max_delay)
+        self._buffer_int = [0] * self._max_delay
+
+        self.delay = starting_delay
+        self.feedback = feedback_gain
+        self.feedback_int = utils.int32(self.feedback * 2**Q_VERB)
+
+        self._buffer_idx = 0
+        self._filterstore = 0
+        self._filterstore_int = 0
         self.damp1 = damping
         self.damp2 = 1 - self.damp1
         self.damp1_int = utils.int32(self.damp1 * 2**Q_VERB)
         self.damp2_int = utils.int32(2**31 - 1 - self.damp1_int)
-        
+
+    def set_delay(self, delay):
+        if delay < self._max_delay:
+            self.delay = delay
+        else:
+            self.delay = self._max_delay
+            Warning("Delay cannot be greater than max delay, setting to max delay")
+        return
+
     def process(self, sample):
 
-        output = self._buffer[self.buffer_idx]
+        output = self._buffer[self._buffer_idx]
 
-        self.filterstore = (output*self.damp2) + (self.filterstore*self.damp1)
+        self._filterstore = (output*self.damp2) + (self._filterstore*self.damp1)
 
-        self._buffer[self.buffer_idx] = sample + (self.filterstore*self._feedback)
+        self._buffer[self.buffer_idx] = sample + (self._filterstore*self._feedback)
         
-        self.buffer_idx += 1
-        if self.buffer_idx >= self._delay:
-            self.buffer_idx = 0
+        self._buffer_idx += 1
+        if self._buffer_idx >= self.delay:
+            self._buffer_idx = 0
 
         return output
 
     def process_xcore(self, sample_int):
         assert isinstance(sample_int, int), "Input sample must be an integer"
 
-        output = self.buffer_int[self.buffer_idx]
+        output = self._buffer_int[self._buffer_idx]
 
-        self.filterstore_int = apply_gain_xcore(self.filterstore_int, self.damp1_int)
-        self.filterstore_int += apply_gain_xcore(output, self.damp2_int)
-        utils.int32(self.filterstore_int)
+        self._filterstore_int = apply_gain_xcore(self._filterstore_int, self.damp1_int)
+        self._filterstore_int += apply_gain_xcore(output, self.damp2_int)
+        utils.int32(self._filterstore_int)
 
-        self.buffer_int[self.buffer_idx] = sample_int 
-        self.buffer_int[self.buffer_idx] += apply_gain_xcore(self.filterstore_int, self.feedback_int)
-        utils.int32(self.buffer_int[self.buffer_idx])
+        self._buffer_int[self._buffer_idx] = sample_int 
+        self._buffer_int[self._buffer_idx] += apply_gain_xcore(self._filterstore_int, self.feedback_int)
+        utils.int32(self._buffer_int[self._buffer_idx])
         
-        self.buffer_idx += 1
-        if self.buffer_idx >= self._delay:
-            self.buffer_idx = 0
+        self._buffer_idx += 1
+        if self._buffer_idx >= self.delay:
+            self._buffer_idx = 0
 
         return output
 
@@ -139,40 +162,69 @@ class freeverb(dspg.dsp_block):
         self.dry_int = utils.int32(self.dry * 2**Q_VERB)
 
         # pregain going into the reverb
-        self.gain = 0.015  
+        self.gain = 0.03125
+        self.out_gain = 0.015/self.gain
+        self.wet *= self.out_gain
+        self.wet_int = utils.int32(self.wet * 2**Q_VERB)
+
         self.gain_int = utils.int32(self.gain * 2**Q_VERB)
 
+        if room_size > 1:
+            raise ValueError("room_size must be less than 1. For larger rooms, increase max_room size")
         self.room_size = room_size
 
         # the magic freeverb delay line lengths are for 44.1kHz, so
         # scale them with sample rate and room size
-        delay_scaling = self.fs/44100 * self.room_size
-        self.comb_lengths = np.array([1116, 1188, 1277, 1356, 1422, 1491, 1557, 1617])
-        self.comb_lengths = (self.comb_lengths*delay_scaling).astype(int)
-        self.ap_lengths = np.array([556, 441, 341, 225])
-        self.ap_lengths = (self.ap_lengths*delay_scaling).astype(int)
+        default_comb_lengths = np.array([1116, 1188, 1277, 1356, 1422, 1491, 1557, 1617])
+        default_ap_lengths = np.array([556, 441, 341, 225])
 
-        self.combs = [comb_fv(self.comb_lengths[0], self.feedback, self.damping),
-                      comb_fv(self.comb_lengths[1], self.feedback, self.damping),
-                      comb_fv(self.comb_lengths[2], self.feedback, self.damping),
-                      comb_fv(self.comb_lengths[3], self.feedback, self.damping),
-                      comb_fv(self.comb_lengths[4], self.feedback, self.damping),
-                      comb_fv(self.comb_lengths[5], self.feedback, self.damping),
-                      comb_fv(self.comb_lengths[6], self.feedback, self.damping),
-                      comb_fv(self.comb_lengths[7], self.feedback, self.damping)]
+        # buffer lengths
+        self.length_scaling = self.fs/44100 * max_room_size
+        self.comb_lengths = (default_comb_lengths*self.length_scaling).astype(int)
+        self.ap_lengths = (default_ap_lengths*self.length_scaling).astype(int)
 
-        self.allpasses = [allpass_fv(self.ap_lengths[0], 0.5),
-                          allpass_fv(self.ap_lengths[1], 0.5),
-                          allpass_fv(self.ap_lengths[2], 0.5),
-                          allpass_fv(self.ap_lengths[3], 0.5)]
+        # buffer delays (always < buffer lengths)
+        comb_delays = (self.comb_lengths * self.room_size).astype(int)
+        ap_delays = (self.ap_lengths * self.room_size).astype(int)
+
+        self.combs = [comb_fv(self.comb_lengths[0], comb_delays[0], self.feedback, self.damping),
+                      comb_fv(self.comb_lengths[1], comb_delays[1], self.feedback, self.damping),
+                      comb_fv(self.comb_lengths[2], comb_delays[2], self.feedback, self.damping),
+                      comb_fv(self.comb_lengths[3], comb_delays[3], self.feedback, self.damping),
+                      comb_fv(self.comb_lengths[4], comb_delays[4], self.feedback, self.damping),
+                      comb_fv(self.comb_lengths[5], comb_delays[5], self.feedback, self.damping),
+                      comb_fv(self.comb_lengths[6], comb_delays[6], self.feedback, self.damping),
+                      comb_fv(self.comb_lengths[7], comb_delays[7], self.feedback, self.damping)]
+
+        self.allpasses = [allpass_fv(self.ap_lengths[0], ap_delays[0], 0.5),
+                          allpass_fv(self.ap_lengths[1], ap_delays[1], 0.5),
+                          allpass_fv(self.ap_lengths[2], ap_delays[2], 0.5),
+                          allpass_fv(self.ap_lengths[3], ap_delays[3], 0.5)]
 
     def get_buffer_lens(self):
         total_buffers = 0
         for cb in self.combs:
-            total_buffers += cb._delay
+            total_buffers += cb._max_delay
         for ap in self.allpasses:
-            total_buffers += ap.delay
+            total_buffers += ap._max_delay
         return total_buffers
+
+    def set_room_size(self, room_size):
+        if room_size > 1:
+            raise ValueError("room_size must be less than 1. For larger rooms, increase max_room size")
+        self.room_size = room_size
+
+        comb_delays = (self.comb_lengths * self.room_size).astype(int)
+        ap_delays = (self.ap_lengths * self.room_size).astype(int)
+
+        for n in range(len(self.combs)):
+            self.combs[n].set_delay(comb_delays[n])
+        
+        for n in range(len(self.allpasses)):
+            self.allpasses[n].set_delay(ap_delays[n])
+
+        return
+
 
     def process(self, sample):
 
@@ -207,6 +259,7 @@ class freeverb(dspg.dsp_block):
 
         return (float(output) * 2**-self.Q_sig)
 
+
 if __name__ == "__main__":
     # hydra_audio_path = os.environ['hydra_audio_PATH']
     # filepath = Path(hydra_audio_path, 'acoustic_team_test_audio',
@@ -218,11 +271,16 @@ if __name__ == "__main__":
     sig = sig/np.max(np.abs(sig))
     sig = sig* (2**31 - 1)/(2**31)
 
-    reverb = freeverb(fs, 1, room_size=1, decay=1, damping=0.0, Q_sig=31)
+    reverb = freeverb(fs, 1, room_size=1, decay=1.0, damping=0.5, Q_sig=31)
     print(reverb.get_buffer_lens())
     
     output = np.zeros_like(sig)
-    for n in range(len(sig)):
+    for n in range(len(sig)//2):
         output[n] = reverb.process_xcore(sig[n])
-    
+
+    reverb.set_room_size(0.5)
+
+    for n in range(len(sig)//2):
+        output[n + len(sig)//2] = reverb.process_xcore(sig[n + len(sig)//2])
+
     sf.write('reverb_out.wav', output, fs)
