4dd5d32a5d24cd93ae8a9a1ef0b7d9dabf62106a
Luciano Martin <lucianom@xmos.com>
2024-04-09 15:04:39 +0100

    Remove duplicated lines

--- lib_audio_dsp/src/dsp/noise_suppressor.c
+++ lib_audio_dsp/src/dsp/noise_suppressor.c
@@ -1,6 +1,5 @@
 // Copyright 2024 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
-// This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 #include "dsp/adsp.h"
 #include "dsp/_helpers/drc_utils.h"
