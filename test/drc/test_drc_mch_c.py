0917a1cb3297173ce866ec43d8c2e84d8845c7a1
Allan Skellett <allanskellett@xmos.com>
2024-05-01 10:09:10 -0400

    update multichannel tolerance, expenders envelope uses 2**Q_SIG - 1

--- test/drc/test_drc_mch_c.py
+++ test/drc/test_drc_mch_c.py
@@ -73,7 +73,9 @@ def test_sidechain_c(in_signal, comp_name, at, rt, threshold, ratio):
   if ratio == 1 or threshold == 0:
     np.testing.assert_allclose(out_c, out_py, rtol=0, atol=0)
   else:
-    np.testing.assert_allclose(out_c, out_py, rtol=0, atol=1.5e-8)
+    # tolerace is the 24b float32 mantissa
+    tol = 2**(np.ceil(np.log2(np.max(out_c))) - 24)
+    np.testing.assert_allclose(out_c, out_py, rtol=0, atol=tol)
 
 if __name__ == "__main__":
   bin_dir.mkdir(exist_ok=True, parents=True)
