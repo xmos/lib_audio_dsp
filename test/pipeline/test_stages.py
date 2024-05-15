20a181d09d8f09c5cd89f1c840d31df11d87c7ef
Luciano Martin <lucianom@xmos.com>
2024-05-15 11:20:34 +0100

    Param generation working for limiter RMS

--- test/pipeline/test_stages.py
+++ test/pipeline/test_stages.py
@@ -19,6 +19,7 @@ from python import build_utils, run_pipeline_xcoreai, audio_helpers
 
 from pathlib import Path
 import numpy as np
+import struct
 
 PKG_DIR = Path(__file__).parent
 APP_DIR = PKG_DIR
@@ -180,6 +181,17 @@ def _test_cascaded_biquad(method, args, frame_size):
 
     do_test(make_p, frame_size)
 
+def generate_test_param_file(stage_name, stage_config):
+    with open(f"build\\src.autogen\\host\\control_test_params.h", "w") as f_op:
+        f_op.write(f"char * stage_name = \"{stage_name}\";\n\n")
+        f_op.write(f"control_data_t control_config[{len(stage_config)}] = {{\n")
+        for cmd_name, cmd_payload in stage_config.items():
+            f_op.write(f"\t{{\n")
+            f_op.write(f"\t\t.cmd_name = \"{stage_name.upper()}_{cmd_name.upper()}\",\n")
+            payload_values = [ "0x{:02X}".format(x&0xFF) for x in struct.unpack('4b', struct.pack('I', cmd_payload))]
+            f_op.write(f"\t\t.payload  = {{{', '.join(payload_values)}}},\n")
+            f_op.write(f"\t}},\n")
+        f_op.write(f"}};")
 
 def test_limiter_rms(frame_size):
     """
@@ -193,6 +205,10 @@ def test_limiter_rms(frame_size):
 
         lim.make_limiter_rms(-6, 0.001, 0.1)
         print(f"\n\n\npipeline in test: {p.resolve_pipeline()['configs'][2]}")
+
+        stage_config = p.resolve_pipeline()['configs'][2]
+        stage_name = "LIMITER_RMS"
+        generate_test_param_file(stage_name, stage_config)
         return p
 
     do_test(make_p, frame_size)
