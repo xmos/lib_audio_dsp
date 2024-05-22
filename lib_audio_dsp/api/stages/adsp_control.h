ec8da2e82984296deea45680b7ff3fa10b545ec0
Huw Percival <huwpercival@xmos.com>
2024-05-22 15:52:20 +0100

    Fix bugs

--- lib_audio_dsp/api/stages/adsp_control.h
+++ lib_audio_dsp/api/stages/adsp_control.h
@@ -43,12 +43,24 @@ typedef enum
     ADSP_CONTROL_BUSY,  ///< Stage has not yet processed the command, call again.
 }adsp_control_status_t;
 
+/// Object used to control a DSP pipeline.
+///
+/// As there may be multiple threads attempting to interact with the DSP pipeline at
+/// the same time, a separate instance of @ref adsp_controller_t must be used by each
+/// to ensure that control can proceed safely.
+///
+/// Initialise each instance of @ref adsp_controller_t with @ref adsp_controller_init.
 typedef struct {
     /// @privatesection
     module_instance_t* modules;
     size_t num_modules;
 } adsp_controller_t;
 
+
+/// Create a DSP controller instance for a particular pipeline.
+///
+/// @param ctrl The controller instance to initialise.
+/// @param pipeline The DSP pipeline that will be controlled with this controller.
 void adsp_controller_init(adsp_controller_t* ctrl, adsp_pipeline_t* pipeline);
 
 /// Initiate a read command by passing in an intialised @ref adsp_stage_control_cmd_t.
@@ -57,8 +69,7 @@ void adsp_controller_init(adsp_controller_t* ctrl, adsp_pipeline_t* pipeline);
 /// abandons the attempt to read before SUCCESS is returned then this will leave the stage in a state
 /// where it can never be read from again.
 ///
-/// @param modules A pointer to the array of modules contained within @ref adsp_pipeline_t.
-/// @param num_modules Size of the num_modules array.
+/// @param ctrl An instance of adsp_controller_t which has been initialised to control the DSP pipeline.
 /// @param cmd An initialised @ref adsp_stage_control_cmd_t.
 /// @return @ref adsp_control_status_t
 adsp_control_status_t adsp_read_module_config(
@@ -70,8 +81,7 @@ adsp_control_status_t adsp_read_module_config(
 ///
 /// Must be called repeatedly with the same cmd until ADSP_CONTROL_SUCCESS is returned.
 ///
-/// @param modules A pointer to the array of modules contained within @ref adsp_pipeline_t.
-/// @param num_modules Size of the @p modules array.
+/// @param ctrl An instance of adsp_controller_t which has been initialised to control the DSP pipeline.
 /// @param cmd An initialised @ref adsp_stage_control_cmd_t.
 /// @return @ref adsp_control_status_t
 adsp_control_status_t adsp_write_module_config(
--- lib_audio_dsp/lib_build_info.cmake
+++ lib_audio_dsp/lib_build_info.cmake
@@ -89,8 +89,8 @@ file(GLOB DSP_C_SOURCES RELATIVE ${CMAKE_CURRENT_LIST_DIR} CONFIGURE_DEPENDS "${
 list(APPEND LIB_C_SRCS ${DSP_C_SOURCES})
 set(LIB_DEPENDENT_MODULES
     "lib_xcore_math(xcommon_cmake)"
-    "lib_logging(>=3.2.0)"
-    "lib_locks(>=2.2.0)"
+    "lib_logging(3.2.0)"
+    "lib_locks(2.2.0)"
 )
 option(LIB_AUDIO_DSP_DISABLE_OPTIMISATION "Set to disable optimisations for better debugging")
 set(_LAD_OPT -O3)
