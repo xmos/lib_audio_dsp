7d54945b5accd67cf877ed6c0d6cb0349b3a5779
Allan Skellett <allanskellett@xmos.com>
2024-05-30 16:06:42 -0400

    Add biquad docstrings

--- python/audio_dsp/stages/biquad.py
+++ python/audio_dsp/stages/biquad.py
@@ -25,7 +25,24 @@ def _ws(locals):
 
 
 class Biquad(Stage):
-    """The stage wrapper for :class:`audio_dsp.dsp.biquad.biquad`."""
+    """
+    A second order biquadratic filter, which can bse used to make many
+    common second order filters. The filter is initialised in a
+    bypass state, and the ``make_*`` methods can be used to calculate the
+    coefficients.
+
+    This Stage implements a direct form 1 biquad filter:
+    ``a0*y[n] = b0*x[n] + b1*x[n-1] + b2*x[n-2] - a1*y[n-1] - a2*y[n-2]``
+
+    For efficiency the biquad coefficients are normalised by ``a0`` and the
+    output ``a`` coefficients multiplied by -1.
+
+    Attributes
+    ----------
+    dsp_block : audio_dsp.dsp.biquad.biquad
+        The dsp block class, see :class:`audio_dsp.dsp.biquad.biquad`
+        for implementation details.
+    """
 
     def __init__(self, **kwargs):
         super().__init__(config=find_config("biquad"), **kwargs)
@@ -45,73 +62,198 @@ class Biquad(Stage):
         return np.array(a * (2**30), dtype=np.int32)
 
     def make_bypass(self) -> "Biquad":
-        """Make this biquad a bypass."""
+        """Make this biquad a bypass, by setting the b0 coefficient to
+        1.
+        """
         self.details = {}
         self.dsp_block = bq.biquad_bypass(self.fs, self.n_in)
         return self
 
     def make_lowpass(self, f: float, q: float) -> "Biquad":
-        """Make this biquad a lowpass."""
+        """Make this biquad a second order low pass filter.
+
+        Parameters
+        ----------
+        f : float
+            cutoff frequency of the filter in Hz.
+        q : float
+            Q factor of the filter roll-off. 0.707 is equivalent to a
+            Butterworth response.
+        """
         self.details = dict(type="low pass", **_ws(locals()))
         self.dsp_block = bq.biquad_lowpass(self.fs, self.n_in, f, q)
         return self
 
     def make_highpass(self, f: float, q: float) -> "Biquad":
-        """Make this biquad a highpass."""
+        """Make this biquad a second order high pass filter.
+
+        Parameters
+        ----------
+        f : float
+            cutoff frequency of the filter in Hz.
+        q : float
+            Q factor of the filter roll-off. 0.707 is equivalent to a
+            Butterworth response.
+        """
         self.details = dict(type="high pass", **_ws(locals()))
         self.dsp_block = bq.biquad_highpass(self.fs, self.n_in, f, q)
         return self
 
     def make_bandpass(self, f: float, bw: float) -> "Biquad":
-        """Make this biquad a bandpass."""
+        """Make this biquad a second order bandpass filter.
+
+        Parameters
+        ----------
+        f : float
+            center frequency of the filter in Hz.
+        bw : float
+            Bandwidth of the filter in octaves.
+        """
         self.details = dict(type="band pass", **_ws(locals()))
         self.dsp_block = bq.biquad_bandpass(self.fs, self.n_in, f, bw)
         return self
 
     def make_bandstop(self, f: float, bw: float) -> "Biquad":
-        """Make this biquad a bandstop."""
+        """Make this biquad a second order bandstop filter.
+
+        Parameters
+        ----------
+        f : float
+            center frequency of the filter in Hz.
+        bw : float
+            Bandwidth of the filter in octaves.
+        """
         self.details = dict(type="band stop", **_ws(locals()))
         self.dsp_block = bq.biquad_bandstop(self.fs, self.n_in, f, bw)
         return self
 
     def make_notch(self, f: float, q: float) -> "Biquad":
-        """Make this biquad a notch."""
+        """Make this biquad a notch filter.
+
+        Parameters
+        ----------
+        f : float
+            center frequency of the filter in Hz.
+        q : float
+            Q factor of the filter.
+        """
         self.details = dict(type="notch", **_ws(locals()))
         self.dsp_block = bq.biquad_notch(self.fs, self.n_in, f, q)
         return self
 
     def make_allpass(self, f: float, q: float) -> "Biquad":
-        """Make this biquad an allpass."""
+        """Make this biquad an all pass filter.
+
+        Parameters
+        ----------
+        f : float
+            center frequency of the filter in Hz.
+        q : float
+            Q factor of the filter.
+        """
         self.details = dict(type="all pass", **_ws(locals()))
         self.dsp_block = bq.biquad_allpass(self.fs, self.n_in, f, q)
         return self
 
     def make_peaking(self, f: float, q: float, boost_db: float) -> "Biquad":
-        """Make this biquad a peaking."""
+        """Make this biquad a peaking filter.
+
+        Parameters
+        ----------
+        f : float
+            center frequency of the filter in Hz.
+        q : float
+            Q factor of the filter.
+        boost_db : float
+            Gain of the filter in decibels.
+        """
         self.details = dict(type="peaking", **_ws(locals()))
         self.dsp_block = bq.biquad_peaking(self.fs, self.n_in, f, q, boost_db)
         return self
 
     def make_constant_q(self, f: float, q: float, boost_db: float) -> "Biquad":
-        """Make this biquad a constant q."""
+        """Make this biquad a peaking filter with constant q.
+
+        Constant Q means that the bandwidth of the filter remains
+        constant as the gain varies. It is commonly used for graphic
+        equalisers.
+
+        Parameters
+        ----------
+        f : float
+            center frequency of the filter in Hz.
+        q : float
+            Q factor of the filter.
+        boost_db : float
+            Gain of the filter in decibels.
+        """
         self.details = dict(type="constant q", **_ws(locals()))
         self.dsp_block = bq.biquad_constant_q(self.fs, self.n_in, f, q, boost_db)
         return self
 
     def make_lowshelf(self, f: float, q: float, boost_db: float) -> "Biquad":
-        """Make this biquad a lowshelf."""
+        """Make this biquad a second order low shelf filter.
+
+        The Q factor is defined in a similar way to standard low pass,
+        i.e. > 0.707 will yield peakiness (where the shelf response does
+        not monotonically change). The level change at f will be
+        boost_db/2.
+
+        Parameters
+        ----------
+        f : float
+            Cutoff frequency of the shelf in Hz, where the gain is
+            boost_db/2
+        q : float
+            Q factor of the filter.
+        boost_db : float
+            Gain of the filter in decibels.
+        """
         self.details = dict(type="lowshelf", **_ws(locals()))
         self.dsp_block = bq.biquad_lowshelf(self.fs, self.n_in, f, q, boost_db)
         return self
 
     def make_highshelf(self, f: float, q: float, boost_db: float) -> "Biquad":
-        """Make this biquad a highshelf."""
+        """Make this biquad a second order high shelf filter.
+
+        The Q factor is defined in a similar way to standard high pass,
+        i.e. > 0.707 will yield peakiness (where the shelf response does
+        not monotonically change). The level change at f will be
+        boost_db/2.
+
+        Parameters
+        ----------
+        f : float
+            Cutoff frequency of the shelf in Hz, where the gain is
+            boost_db/2
+        q : float
+            Q factor of the filter.
+        boost_db : float
+            Gain of the filter in decibels.
+        """
         self.details = dict(type="highshelf", **_ws(locals()))
         self.dsp_block = bq.biquad_highshelf(self.fs, self.n_in, f, q, boost_db)
         return self
 
     def make_linkwitz(self, f0: float, q0: float, fp: float, qp: float) -> "Biquad":
-        """Make this biquad a linkwitz."""
+        """Make this biquad a Linkwitz Transform biquad filter.
+
+        The Linkwitz Transform is commonly used to change the low
+        frequency roll off slope of a loudspeaker. When applied to a
+        loudspeaker, it will change the cutoff frequency from f0 to fp,
+        and the quality factor from q0 to qp.
+
+        Parameters
+        ----------
+        f0 : float
+            The original cutoff frequency of the filter in Hz.
+        q0 : float
+            The original quality factor of the filter at f0.
+        fp : float
+            The target cutoff frequency for the filter in Hz.
+        qp : float
+            The target quality factor for the filter.
+        """
         self.details = dict(type="linkwitz", **_ws(locals()))
         self.dsp_block = bq.biquad_linkwitz(self.fs, self.n_in, f0, q0, fp, qp)
         return self
