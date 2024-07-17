19e921fa7a66582f86c622002982448855d41ded
xalbertoisorna” <albertoisorna@xmos.com>
2024-07-17 15:47:24 +0100

    fixing relative path

--- tools/update-requirements.py
+++ tools/update-requirements.py
@@ -18,8 +18,7 @@ repo_root = cwd.parent.absolute()
 
 def get_header_text():
     py_major, py_minor = sys.version_info.major, sys.version_info.minor
-    pip_version = pip.__version__
-    pip_version_major = pip_version.split(".", maxsplit=2)[0]
+    pip_version_major = (pip.__version__).split(".", maxsplit=2)[0]
     header_text = f"# python_version {py_major}.{py_minor}\n"
     header_text += f"# pip_version {pip_version_major}.*\n"
     return header_text
@@ -28,12 +27,14 @@ def get_header_text():
 def main():
     # configure pip-tools header comment
     os.environ["CUSTOM_COMPILE_COMMAND"] = f"./tools/update-requirements.py"
-
+    
     # glob each .in and produce a .txt
     in_files = cwd.glob("*.in")
     for req_in in in_files:
-        req_dst = repo_root / req_in.with_suffix(".txt").name
-        cmd = f"pip-compile {req_in} -o {req_dst}"
+        req_in = req_in.relative_to(repo_root) # needed for relative path output
+        req_dst = (repo_root / req_in.with_suffix(".txt").name).relative_to(repo_root)
+        opts = "--no-emit-trusted-host --no-emit-index-url"
+        cmd = f"pip-compile {req_in} -o {req_dst} {opts}"
         subprocess.run(cmd, shell=True, check=True, cwd=repo_root, timeout=200)
         # add header to req_dst
         header_text = get_header_text()
