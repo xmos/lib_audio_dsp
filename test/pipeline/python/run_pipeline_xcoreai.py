ff1004cd0ffcc75afa52fe6e4da5d49668387913
Allan Skellett <allanskellett@xmos.com>
2024-09-20 15:18:47 +0100

    use xtagctl

--- test/pipeline/python/run_pipeline_xcoreai.py
+++ test/pipeline/python/run_pipeline_xcoreai.py
@@ -8,25 +8,26 @@ import scipy.io.wavfile
 import pathlib
 from filelock import FileLock
 import time
+import xtagctl
 
 FORCE_ADAPTER_ID = None
 
 class XtagError(RuntimeError):
     pass
 
-def xtag_getter():
-    timeout = time.time() + 60*10   # 10 minutes from now
-    while True:
-        time.sleep(0.1)
-        if time.time() > timeout:
-            raise RuntimeError("No Xtag available before timeout")
-        with FileLock("xtag.lock"):
-            try:
-                adapter_id = get_adapter_id()
-                print("Got XTAG %s" % adapter_id)
-                return adapter_id
-            except XtagError:
-                continue
+# def xtag_getter():
+#     timeout = time.time() + 60*10   # 10 minutes from now
+#     while True:
+#         time.sleep(0.1)
+#         if time.time() > timeout:
+#             raise RuntimeError("No Xtag available before timeout")
+#         with FileLock("xtag.lock"):
+#             try:
+#                 adapter_id = get_adapter_id()
+#                 print("Got XTAG %s" % adapter_id)
+#                 return adapter_id
+#             except XtagError:
+#                 continue
 
 def get_adapter_id():
     # check the --adapter-id option
@@ -114,17 +115,20 @@ def run(xe, input_file, output_file, num_out_channels, pipeline_stages=1, return
     with open("args.txt", "w") as fp:
         fp.write(args)
 
-    adapter_id = xtag_getter()
-    print("Running on adapter_id ",adapter_id)
-
-    if return_stdout == False:
-        xscope_fileio.run_on_target(adapter_id, xe)
-    else:
-        with open("stdout.txt", "w+") as ff:
-            xscope_fileio.run_on_target(adapter_id, xe, stdout=ff)
-            ff.seek(0)
-            stdout = ff.readlines()
-        return stdout
+    # adapter_id = xtag_getter()
+    # print("Running on adapter_id ",adapter_id)
+    with xtagctl.acquire("XCORE-AI-EXPLORER") as adapter_id:
+        # Reset adapter before running test
+        xtagctl.reset_adapter(adapter_id)
+        time.sleep(2) # Wait for adapter to enumerate
+        if return_stdout == False:
+            xscope_fileio.run_on_target(adapter_id, xe)
+        else:
+            with open("stdout.txt", "w+") as ff:
+                xscope_fileio.run_on_target(adapter_id, xe, stdout=ff)
+                ff.seek(0)
+                stdout = ff.readlines()
+            return stdout
 
 if __name__ == "__main__":
     args = parse_arguments()
