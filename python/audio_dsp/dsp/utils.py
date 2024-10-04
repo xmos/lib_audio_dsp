3b162599417071051dbacd6d3f8e52ec99bc15d6
Allan Skellett <allanskellett@xmos.com>
2024-10-04 08:58:48 +0100

    Better Jenkins balance , remove duplicated function, ensure Q format is int

--- python/audio_dsp/dsp/utils.py
+++ python/audio_dsp/dsp/utils.py
@@ -256,13 +256,6 @@ def float_list_to_int32(x, Q_sig=31) -> typing.List[int]:
     return [int32(round(item * (1 << Q_sig))) for item in x]
 
 
-def float_list_to_int32(x, Q_sig=31) -> typing.List[int]:
-    """Round and scale a list of floating point numbers to a list of
-    int32s in a given Q format.
-    """
-    return [int32(round(item * (1 << Q_sig))) for item in x]
-
-
 def int32_to_float(x: int, Q_sig: int = 31) -> float:
     """Convert an int32 number to floating point, given its Q format."""
     # Note this means the max value is 0.99999999953
