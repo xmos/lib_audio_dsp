d53b3f7d243ef0d3ccfa841a1a81d4610351892e
Allan Skellett <allanskellett@xmos.com>
2025-02-06 14:44:43 +0000

    temp add back old slew

--- lib_audio_dsp/api/dsp/biquad.h
+++ lib_audio_dsp/api/dsp/biquad.h
@@ -37,25 +37,25 @@ void adsp_biquad_slew_coeffs(
   int32_t channels
 );
 
-// /**
-//  * @brief Biquad filter with slew.
-//  *  This function implements a biquad filter with slew. 
-//  *  The filter is implemented as a direct form 1.
-//  *  The coeffs are exponentially slewed towards the target_coeffs.
-//  *  This is can be used for real time adjustable biquads.
-//  * 
-//  * @param new_sample      New sample to be filtered
-//  * @param coeffs          Filter coefficients
-//  * @param target_coeffs   Target filter coefficients
-//  * @param state           Filter state
-//  * @param lsh             Left shift compensation value
-//  * @param slew_shift      Shift value used in the exponential slew
-//  * @return int32_t        Filtered sample
-//  */
-// int32_t adsp_biquad_slew(
-//   int32_t new_sample,
-//   q2_30 coeffs[8],
-//   q2_30 target_coeffs[8],
-//   int32_t** states,
-//   left_shift_t lsh,
-//   int32_t slew_shift);
+/**
+ * @brief Biquad filter with slew.
+ *  This function implements a biquad filter with slew. 
+ *  The filter is implemented as a direct form 1.
+ *  The coeffs are exponentially slewed towards the target_coeffs.
+ *  This is can be used for real time adjustable biquads.
+ * 
+ * @param new_sample      New sample to be filtered
+ * @param coeffs          Filter coefficients
+ * @param target_coeffs   Target filter coefficients
+ * @param state           Filter state
+ * @param lsh             Left shift compensation value
+ * @param slew_shift      Shift value used in the exponential slew
+ * @return int32_t        Filtered sample
+ */
+int32_t adsp_biquad_slew(
+  int32_t new_sample,
+  q2_30 coeffs[8],
+  q2_30 target_coeffs[8],
+  int32_t state[8],
+  left_shift_t lsh,
+  int32_t slew_shift);
