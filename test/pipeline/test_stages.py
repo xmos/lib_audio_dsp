bf12aa83d5744124710cb038ee40b27cf70807b7
Allan Skellett <allanskellett@xmos.com>
2025-02-17 15:18:39 +0000

    rename test_stages flags

--- test/pipeline/test_stages.py
+++ test/pipeline/test_stages.py
@@ -57,7 +57,7 @@ def generate_ref(sig, ref_module, pipeline_channels, frame_size):
     return out_py_int
 
 
-def do_test(make_p, tune_p, dut_frame_size, folder_name, skip_default=False):
+def do_test(default_pipeline, tuned_pipeline, dut_frame_size, folder_name, skip_default=False):
     """
     Run stereo file into app and check the output matches
     using in_ch and out_ch to decide which channels to compare
@@ -68,10 +68,10 @@ def do_test(make_p, tune_p, dut_frame_size, folder_name, skip_default=False):
 
     Parameters
     ----------
-    make_p: function
+    default_pipeline: function
         function that takes a frame size and returns a pipeline which
         has that frame size as the input. It uses the default configuration values.
-    tune_p: function
+    tuned_pipeline: function
         function that takes a frame size, returns a pipeline which
         has that frame size as the input, and tunes the pipelines with the desired
         configuration values
@@ -84,8 +84,8 @@ def do_test(make_p, tune_p, dut_frame_size, folder_name, skip_default=False):
 
     with FileLock(build_utils.PIPELINE_BUILD_LOCK):
 
-        for func_p in [make_p, tune_p]:
-            # Exit if tune_p is not defined
+        for func_p in [default_pipeline, tuned_pipeline]:
+            # Exit if tuned_pipeline is not defined
             if not func_p:
                 continue
 
@@ -94,11 +94,11 @@ def do_test(make_p, tune_p, dut_frame_size, folder_name, skip_default=False):
 
             out_dir = None
 
-            # Generate uninitialized stages for make_p, only if tune_p is defined
-            if (func_p == make_p and tune_p):
-                out_dir = "dsp_pipeline_uninitialized"
+            # Generate uninitialized stages for default_pipeline, only if tuned_pipeline is defined
+            if func_p == default_pipeline and tuned_pipeline:
+                out_dir = "dsp_pipeline_tuned"
             else:
-                out_dir = "dsp_pipeline_initialized"
+                out_dir = "dsp_pipeline_default"
             generate_dsp_main(dut_p, out_dir=BUILD_DIR / out_dir)
 
         n_samps, rate = 1024, 48000
@@ -122,8 +122,8 @@ def do_test(make_p, tune_p, dut_frame_size, folder_name, skip_default=False):
 
         audio_helpers.write_wav(infile, rate, sig)
         
-        # The reference function should be always tune_p, it is make_p if tune_p is not defined
-        ref_func_p = tune_p if tune_p else make_p
+        # The reference function should be always tuned_pipeline, it is default_pipeline if tuned_pipeline is not defined
+        ref_func_p = tuned_pipeline if tuned_pipeline else default_pipeline
 
         ref_p = [ref_func_p(s) for s in TEST_FRAME_SIZES]
         out_py_int_all = [
@@ -131,20 +131,24 @@ def do_test(make_p, tune_p, dut_frame_size, folder_name, skip_default=False):
             for p, fr in zip(ref_p, TEST_FRAME_SIZES)
         ]
 
-        for target in ["default", "control_commands"]:
-            # Do not run the control test if tune_p is not defined
-            if not tune_p and target == "control_commands":
+        for target in ["default", "tuned"]:
+            # Do not run the control test if tuned_pipeline is not defined
+            if not tuned_pipeline and target == "tuned":
                 continue
             # Build pipeline test executable. This will download xscope_fileio if not present
-            build_utils.build(APP_DIR, BUILD_DIR, target)
+            if target == "tuned":
+                build_utils.build(APP_DIR, BUILD_DIR, "control_commands")
+            else:
+                build_utils.build(APP_DIR, BUILD_DIR, "default")
+
             # old_xe = APP_DIR / f"bin/{target}/pipeline_test_{target}.xe"
             # new_xe = app_dir / f"bin/{target}/pipeline_test_{target}.xe"
         os.makedirs(app_dir / "bin", exist_ok=True)
         shutil.copytree(APP_DIR / "bin", app_dir / "bin", dirs_exist_ok=True)
 
-    for target in ["default", "control_commands"]:
-        # Do not run the control test if tune_p is not defined
-        if not tune_p and target == "control_commands":
+    for target in ["default", "tuned"]:
+        # Do not run the control test if tuned_pipeline is not defined
+        if not tuned_pipeline and target == "tuned":
             continue
         if target == "default" and skip_default:
             continue
@@ -260,7 +264,7 @@ def generate_test_param_file(config_name, stage_config):
         ("make_bandstop", [1000, 0.707]),
         ("make_notch", [1000, 0.707]),
         ("make_allpass", [1000, 0.707]),
-        ("make_peaking", [1000, 0.707, -6]),
+        ("default_pipelineeaking", [1000, 0.707, -6]),
         ("make_constant_q", [1000, 0.707, -6]),
         ("make_lowshelf", [1000, 0.707, -6]),
         ("make_highshelf", [1000, 0.707, -6]),
@@ -272,15 +276,16 @@ def test_biquad(method, args, frame_size):
     Test the biquad stage filters the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p, i = Pipeline.begin(channels, frame_size=fr)
         o = p.stage(Biquad, i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
+
         bq_method = getattr(p["control"], method)
 
         # Set initialization parameters of the stage
@@ -294,7 +299,7 @@ def test_biquad(method, args, frame_size):
         return p
 
     folder_name = f"biquad_{frame_size}_{method[5:]}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 
 @pytest.mark.parametrize(
@@ -307,7 +312,7 @@ def test_biquad(method, args, frame_size):
         ("make_bandstop", [1000, 0.707]),
         ("make_notch", [1000, 0.707]),
         ("make_allpass", [1000, 0.707]),
-        ("make_peaking", [1000, 0.707, -6]),
+        ("default_pipelineeaking", [1000, 0.707, -6]),
         ("make_constant_q", [1000, 0.707, -6]),
         ("make_lowshelf", [1000, 0.707, -6]),
         ("make_highshelf", [1000, 0.707, 10]),
@@ -319,15 +324,15 @@ def test_biquad_slew(method, args, frame_size):
     Test the biquad stage filters the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p, i = Pipeline.begin(channels, frame_size=fr)
         o = p.stage(BiquadSlew, i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         bq_method = getattr(p["control"], method)
 
@@ -345,7 +350,7 @@ def test_biquad_slew(method, args, frame_size):
 
     # only run the control test, as there is no way to init python pipeline differently
     # for default & control tests
-    do_test(make_p, tune_p, frame_size, folder_name, skip_default=True)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name, skip_default=True)
 
 
 filter_spec = [
@@ -365,7 +370,7 @@ filter_spec = [
     [
         ("make_butterworth_highpass", [8, 1000]),
         ("make_butterworth_lowpass", [8, 1000]),
-        ("make_parametric_eq", [filter_spec]),
+        ("default_pipelinearametric_eq", [filter_spec]),
     ],
 )
 def test_cascaded_biquad(method, args, frame_size):
@@ -373,15 +378,15 @@ def test_cascaded_biquad(method, args, frame_size):
     Test the biquad stage filters the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o= p.stage(CascadedBiquads, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         bq_method = getattr(p["control"], method)
@@ -395,7 +400,7 @@ def test_cascaded_biquad(method, args, frame_size):
         return p
 
     folder_name = f"cbq_{frame_size}_{method[5:]}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 @pytest.mark.group0
 def test_limiter_rms(frame_size):
@@ -403,15 +408,15 @@ def test_limiter_rms(frame_size):
     Test the limiter stage limits the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(LimiterRMS, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p= make_p(fr)
+    def tuned_pipeline(fr):
+        p= default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].make_limiter_rms(-6, 0.001, 0.1)
@@ -421,7 +426,7 @@ def test_limiter_rms(frame_size):
         return p
 
     folder_name = f"limiterrms_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 
 def test_limiter_peak(frame_size):
@@ -429,15 +434,15 @@ def test_limiter_peak(frame_size):
     Test the limiter stage limits the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(LimiterPeak, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].make_limiter_peak(-6, 0.001, 0.1)
@@ -447,7 +452,7 @@ def test_limiter_peak(frame_size):
         return p
 
     folder_name = f"limiterpeak_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 @pytest.mark.group0
 def test_hard_limiter_peak(frame_size):
@@ -455,15 +460,15 @@ def test_hard_limiter_peak(frame_size):
     Test the limiter stage limits the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(HardLimiterPeak, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].make_hard_limiter_peak(-6, 0.001, 0.1)
@@ -473,7 +478,7 @@ def test_hard_limiter_peak(frame_size):
         return p
 
     folder_name = f"hardlimiterpeak_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 
 def test_clipper(frame_size):
@@ -481,15 +486,15 @@ def test_clipper(frame_size):
     Test the clipper stage clips the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(Clipper, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].make_clipper(-6)
@@ -499,7 +504,7 @@ def test_clipper(frame_size):
         return p
 
     folder_name = f"clipper_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 @pytest.mark.group0
 def test_compressor(frame_size):
@@ -507,15 +512,15 @@ def test_compressor(frame_size):
     Test the compressor stage compresses the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(CompressorRMS, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].make_compressor_rms(2, -6, 0.001, 0.1)
@@ -525,22 +530,22 @@ def test_compressor(frame_size):
         return p
 
     folder_name = f"compressor_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 def test_noise_gate(frame_size):
     """
     Test the noise gate stage gates the noise the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(NoiseGate, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].make_noise_gate(-6, 0.001, 0.1)
@@ -550,22 +555,22 @@ def test_noise_gate(frame_size):
         return p
 
     folder_name = f"noise_gate_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 def test_noise_suppressor_expander(frame_size):
     """
     Test the noise suppressor (expander) stage suppress the noise the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(NoiseSuppressorExpander, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].make_noise_suppressor_expander(2, -6, 0.001, 0.1)
@@ -575,7 +580,7 @@ def test_noise_suppressor_expander(frame_size):
         return p
 
     folder_name = f"noise_suppressor_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 
 def test_volume(frame_size):
@@ -583,12 +588,12 @@ def test_volume(frame_size):
     Test the volume stage amplifies the same in Python and C
     """
 
-    # The gain_dB and mute_state must match in both make_p() and tune_p().
+    # The gain_dB and mute_state must match in both default_pipeline() and tuned_pipeline().
     # Those values are used to compute the starting gain, and it must match in both applications
     gain_dB = -8
     mute_state = 0
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(VolumeControl, p.i, label="control")
         p.set_outputs(o)
@@ -596,8 +601,8 @@ def test_volume(frame_size):
         p["control"].set_mute_state(mute_state)
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         p["control"].make_volume_control(gain_dB, 10, mute_state)
         stage_config = p["control"].get_config()
@@ -605,22 +610,22 @@ def test_volume(frame_size):
         return p
 
     folder_name = f"volume_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 def test_fixed_gain(frame_size):
     """
     Test the volume stage amplifies the same in Python and C
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(FixedGain, p.i, label="control")
         p.set_outputs(o)
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].set_gain(-8)
@@ -630,7 +635,7 @@ def test_fixed_gain(frame_size):
         return p
 
     folder_name = f"fixed_gain_{frame_size}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 @pytest.mark.group0
 @pytest.mark.parametrize("pregain, mix", [
@@ -644,7 +649,7 @@ def test_reverb(frame_size, pregain, mix):
     """
 
 
-    def make_p(fr):
+    def default_pipeline(fr):
         reverb_test_channels = 1  # Reverb expects only 1 channel
         p = Pipeline(reverb_test_channels, frame_size=fr)
         o = p.stage(ReverbRoom, p.i, label="control")
@@ -652,8 +657,8 @@ def test_reverb(frame_size, pregain, mix):
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         if mix:
@@ -672,7 +677,7 @@ def test_reverb(frame_size, pregain, mix):
         return p
 
     folder_name = f"reverbroom_{frame_size}_{pregain}_{int(mix)}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 
 @pytest.mark.parametrize("pregain, mix", [
@@ -685,7 +690,7 @@ def test_reverb_plate(frame_size, pregain, mix):
     Test Reverb stage
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         reverb_test_channels = 2  # Reverb expects only 2 channel
         p = Pipeline(reverb_test_channels, frame_size=fr)
         o = p.stage(ReverbPlateStereo, p.i, label="control")
@@ -693,8 +698,8 @@ def test_reverb_plate(frame_size, pregain, mix):
 
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         if mix:
@@ -715,7 +720,7 @@ def test_reverb_plate(frame_size, pregain, mix):
         return p
 
     folder_name = f"reverbplate_{frame_size}_{pregain}_{int(mix)}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 
 @pytest.mark.parametrize("change_delay", [5, 0])
@@ -724,7 +729,7 @@ def test_delay(frame_size, change_delay):
     Test Delay stage
     """
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(
             Delay, p.i, max_delay=15, starting_delay=10, label="control"
@@ -732,8 +737,8 @@ def test_delay(frame_size, change_delay):
         p.set_outputs(o)
         return p
 
-    def tune_p(fr):
-        p = make_p(fr)
+    def tuned_pipeline(fr):
+        p = default_pipeline(fr)
 
         # Set initialization parameters of the stage
         p["control"].set_delay(change_delay)
@@ -743,7 +748,7 @@ def test_delay(frame_size, change_delay):
         return p
 
     folder_name = f"delay_{frame_size}_{change_delay}"
-    do_test(make_p, tune_p, frame_size, folder_name)
+    do_test(default_pipeline, tuned_pipeline, frame_size, folder_name)
 
 @pytest.fixture(scope="session", autouse=True)
 def make_coeffs():
@@ -770,14 +775,11 @@ def test_fir(frame_size, filter_name):
     """
     filter_path = Path(Path(__file__).parent / "autogen", filter_name)
 
-    def make_p(fr):
+    def default_pipeline(fr):
         p = Pipeline(channels, frame_size=fr)
         o = p.stage(FirDirect, p.i, coeffs_path=filter_path)
         p.set_outputs(o)
         return p
 
     folder_name = f"fir_{frame_size}_{filter_name[:5]}"
-    do_test(make_p, None, frame_size, folder_name)
-
-if __name__ == "__main__":
-    test_biquad_slew("make_lowpass", [1000, 0.707], 1)
+    do_test(default_pipeline, None, frame_size, folder_name)
