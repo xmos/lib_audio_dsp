968ef494473948ab7d41e0096a1d86dd391f7f80
Allan Skellett <allanskellett@xmos.com>
2025-04-04 11:05:57 +0100

    small speed up

--- lib_audio_dsp/src/dsp/graphic_eq.c
+++ lib_audio_dsp/src/dsp/graphic_eq.c
@@ -48,20 +48,22 @@ int32_t adsp_graphic_eq_10b(int32_t new_sample,
 {
     int32_t ah = 0, al = 1 << (Q_GEQ - 1);
     int state_idx = 0;
+    int32_t polarity = -1;
 
+    #pragma unroll
     for(unsigned n = 0; n < 10; n++)
     {
-        int32_t this_band = new_sample;
+        int32_t this_band;
         
-        this_band = adsp_biquad(this_band, &coeffs[5 * n], &state[state_idx], 0);
+        this_band = adsp_biquad(new_sample, &coeffs[5 * n], &state[state_idx], 0);
         state_idx += 8;
         this_band = adsp_biquad(this_band, &coeffs[5 * n], &state[state_idx], 0);
         state_idx += 8;
 
-        // alternate phase of bands
-        int32_t this_gain = n % 2 == 0 ? gains[n] : -gains[n];
+        // alternate summing and subtracting bands
+        polarity *= -1;
+        int32_t this_gain = gains[n] * polarity;
         asm("maccs %0, %1, %2, %3": "=r" (ah), "=r" (al): "r" (this_band), "r" (this_gain), "0" (ah), "1" (al));
-
     }
 
     asm("lsats %0, %1, %2": "=r" (ah), "=r" (al): "r" (Q_GEQ), "0" (ah), "1" (al));
