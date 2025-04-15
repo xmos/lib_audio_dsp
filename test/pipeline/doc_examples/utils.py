bed5a856c1fff59f8beba6e1b68e74357a636fe0
Allan Skellett <allanskellett@xmos.com>
2025-04-15 09:14:28 +0100

    Remove duplicated copyright notices

--- test/pipeline/doc_examples/utils.py
+++ test/pipeline/doc_examples/utils.py
@@ -1,9 +1,6 @@
 # Copyright 2024-2025 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
-# Copyright 2024 XMOS LIMITED.
-# This Software is subject to the terms of the XMOS Public Licence: Version 1.
-
 """
 if docs path change, update them here
 """
--- test/pipeline/stages/__init__.py
+++ test/pipeline/stages/__init__.py
@@ -1,3 +1,2 @@
 # Copyright 2024-2025 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-# This Software is subject to the terms of the XMOS Public Licence: Version 1.
